c7e272cf8d3d27324232f6869cc0ea7415746af2
Nain Tornez <nasatome@gmail.com>
2021-03-10 14:07:33 -0600

    fix: the input device is not a TTY

--- docker/cronjob.sh
+++ docker/cronjob.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "CRON from cronjob.sh"
-/usr/local/bin/docker-compose -f /opt/prj/docker-firefly-iii/docker/docker-compose.yml exec -u www-data fireflyiii /usr/bin/php /var/www/html/artisan firefly:cron
+/usr/local/bin/docker-compose -f /opt/prj/docker-firefly-iii/docker/docker-compose.yml exec -T -u www-data fireflyiii /usr/bin/php /var/www/html/artisan firefly:cron
 echo "=====End===="
