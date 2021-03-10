7d04c6087953be8beb78a601a25e7b1bf544c507
Nain Tornez <nasatome@gmail.com>
2021-03-10 14:09:16 -0600

    fix: paths

--- docker/cronjob.sh
+++ docker/cronjob.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "CRON from cronjob.sh"
-/usr/local/bin/docker-compose -f /opt/prj/docker-firefly-iii/docker/docker-compose.yml exec -T -u www-data fireflyiii /usr/bin/php /var/www/html/artisan firefly:cron
+/usr/local/bin/docker-compose -f /opt/prj/docker-firefly-iii/docker/docker-compose.yml exec -T -u www-data fireflyiii php artisan firefly:cron
 echo "=====End===="
