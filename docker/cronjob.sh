b369e1432a837962a6801aad02009f9912b9be12
Nain Tornez <nasatome@gmail.com>
2021-03-10 16:14:07 -0600

    fix: add crontab example
    https://github.com/firefly-iii/firefly-iii/issues/2372

--- docker/cronjob.sh
+++ docker/cronjob.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# crontab -e example:
+# 0 3 * * * /opt/prj/docker-firefly-iii/docker/cronjob.sh >> /opt/prj/docker-firefly-iii/docker/cron.log 2>&1
 now=$(date)
 echo "=====$now====="
 /usr/local/bin/docker-compose -f /opt/prj/docker-firefly-iii/docker/docker-compose.yml exec -T -u www-data fireflyiii php artisan firefly:cron
