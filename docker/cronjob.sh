f59b1ea55f535b180816156e379c57e0a268a667
Nain Tornez <nasatome@gmail.com>
2021-03-10 14:13:51 -0600

    fix: add date to echo script

--- docker/cronjob.sh
+++ docker/cronjob.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "CRON from cronjob.sh"
+now=$(date)
+echo "=====$now====="
 /usr/local/bin/docker-compose -f /opt/prj/docker-firefly-iii/docker/docker-compose.yml exec -T -u www-data fireflyiii php artisan firefly:cron
-echo "=====End===="
